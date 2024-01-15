a782940c82c1b22619b9b7037bceb8ada614ba09
Alan Spark <arisiru@hotmail.com>
2024-01-15 20:15:08 +0100

    add: modularization of tranformation, healthy patients inference, datest oda

---
+++ src/utils/input.py
@@ -0,0 +1,120 @@
+import json
+import os
+
+import tensorflow as tf
+
+import numpy as np
+
+import utils.transformations as transformations
+
+CONFIG = {}
+
+
+def setup(root_path, seq_max_len, num_exercises, num_flag_bs):
+    CONFIG['ROOT_PATH'] = root_path
+
+    setting_path = os.path.join(CONFIG['ROOT_PATH'], 'src', 'settings')
+    CONFIG['ALL_REGIONS'] = json.load(open(os.path.join(setting_path, 'all_regions.json')))
+    CONFIG['REGIONS'] = json.load(open(os.path.join(setting_path, 'pois_region.json')))
+    CONFIG['BASES'] = json.load(open(os.path.join(setting_path, 'pois_base.json')))
+    
+    CONFIG['SEQ_MAX_LEN'] = seq_max_len
+
+    CONFIG['NUM_EXERCISES'] = num_exercises
+    CONFIG['NUM_FLAG_BS'] = num_flag_bs
+
+    return CONFIG
+
+def feature_engineering_sequence(poi, isrebase=False, poi_name='', pois={}, settings={}):
+    new_sequences = []    
+    
+    if isrebase:
+        xs = transformations.rebase(poi['xs'], 'xs', poi_name, pois, CONFIG['BASES'])
+        ys = transformations.rebase(poi['ys'], 'ys', poi_name, pois, CONFIG['BASES'])
+        zs = transformations.rebase(poi['zs'], 'zs', poi_name, pois, CONFIG['BASES'])
+    else:
+        xs = poi['xs']
+        ys = poi['ys']
+        zs = poi['zs']
+        
+    #original
+    if settings['coordinates'] or isrebase:
+        new_sequences.append(xs)
+        new_sequences.append(ys)
+        new_sequences.append(zs)
+
+    if not isrebase or settings['transformation_to_rebase']:
+        #normalize by start
+        if settings['normalize_by_start']:
+            new_sequences.append(transformations.normalize_by_start(xs)) 
+            new_sequences.append(transformations.normalize_by_start(ys))
+            new_sequences.append(transformations.normalize_by_start(zs))
+        #normalize    
+        if settings['normalize']:
+            new_sequences.append(transformations.normalize(xs)) 
+            new_sequences.append(transformations.normalize(ys))
+            new_sequences.append(transformations.normalize(zs))
+        #direction
+        if settings['direction']:
+            new_sequences.append(transformations.direction_angles(xs, ys, zs))
+        #distance
+        if settings['distance']:
+            new_sequences.append(transformations.distance(xs, ys, zs))
+    
+    return new_sequences
+
+def build_region(pois, region, settings):
+    exercise_sequence_region = []
+    
+    for poi_name in sorted(CONFIG['REGIONS'].keys()):
+        if CONFIG['REGIONS'][poi_name] == region or region == 'global':
+            sequences = pois[poi_name]
+            exercise_sequence_region.extend(feature_engineering_sequence(sequences, isrebase=False, settings=settings))
+            exercise_sequence_region.extend(feature_engineering_sequence(sequences, isrebase=True, poi_name=poi_name, pois=pois, settings=settings))
+    
+
+    exercise_sequence_region = tf.keras.preprocessing.sequence.pad_sequences(
+        exercise_sequence_region,
+        padding="pre",
+        maxlen=CONFIG['SEQ_MAX_LEN'],
+        dtype='float32')
+    
+    return exercise_sequence_region
+
+
+def build_meta(exercise):
+    x_a_1 = [0] * CONFIG['NUM_EXERCISES']
+    x_a_2 = [0] * CONFIG['NUM_FLAG_BS']
+    exercise_id = None
+    if 'id' in exercise['meta']:
+        exercise_id = exercise['meta']['id']
+    else:
+        exercise_id = int(exercise['meta']['tag'].split('_')[0]) - 1
+    x_a_1[exercise_id] = 1
+    x_a_2[exercise['meta']['flag_before_surgery']] = 1
+
+    return x_a_1 + x_a_2
+
+
+def exercise_to_input(file_path, settings):
+    exercise = json.load(open(file_path, 'r'))
+
+    # build regions 
+    exercise_sequence_global_region = build_region(exercise['pois'], 'global', settings)
+    exercise_sequence_frontal_region = build_region(exercise['pois'], 'frontal', settings)
+    exercise_sequence_oral_region = build_region(exercise['pois'], 'oral', settings)
+    exercise_sequence_orbital_region = build_region(exercise['pois'], 'orbital', settings)
+        
+    # build meta
+    exercise_meta = build_meta(exercise)
+
+    evaluation = exercise['meta']['evaluation']
+
+    return [
+            exercise_meta, 
+            exercise_sequence_global_region, 
+            exercise_sequence_frontal_region,
+            exercise_sequence_oral_region,
+            exercise_sequence_orbital_region,
+            evaluation
+           ]
---
+++ src/utils/transformations.py
@@ -0,0 +1,48 @@
+import math
+import numpy as np
+
+def rescale(value, minimum, maximum):
+    return (value - minimum) / (maximum - minimum)
+
+def normalize(s):
+    s_minimum = min(s)
+    s_maximum = max(s)
+    
+    if (s_maximum - s_minimum) == 0:
+        return s
+    
+    return [rescale(x, s_minimum, s_maximum) for x in s]
+
+def unit_vector(vector):
+    if np.linalg.norm(vector) == 0:
+        return np.nan
+    
+    return vector / np.linalg.norm(vector)
+
+def angle_between_two_points(p1, p2):
+    p1_u = unit_vector(p1)
+    p2_u = unit_vector(p2)
+    result = np.arccos(np.clip(np.dot(p1_u, p2_u), -1.0, 1.0))
+    return 0.0 if np.isnan(result) else result
+
+def direction_angles(xs, ys, zs):
+    angles = [0]
+    for i in range(1, len(xs)):
+        angles.append(angle_between_two_points([xs[i-1], ys[i-1], zs[i-1]], [xs[i], ys[i], zs[i]]))
+        
+    return angles
+
+def normalize_by_start(s):
+    return [x - s[0] for x in s]
+
+def distance(xs, ys, zs):
+    distance = [0]
+    for i in range(1, len(xs)):
+        distance.append(math.dist([xs[i-1], ys[i-1], zs[i-1]], [xs[i], ys[i], zs[i]]))
+    
+    return distance
+
+def rebase(s, s_name, poi_name, pois, bases):
+    base_name = bases[poi_name]
+    base_s = pois[base_name][s_name]
+    return[x - bx for x, bx in zip(s, base_s)]
